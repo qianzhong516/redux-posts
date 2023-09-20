ed33e912a2068680189d25f48943c009a45f07c1
qianzhong516 <zhongqian516@gmail.com>
2023-09-20 18:22:41 +1000

    add notifications

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postReducer from '../features/posts/postSlice'
 import userReducer from '../features/users/userSlice'
+import notificationReducer from '../features/notifications/notificationSlice'
 
 export default configureStore({
   reducer: {
     posts: postReducer,
     users: userReducer,
+    notifications: notificationReducer,
   },
 })
